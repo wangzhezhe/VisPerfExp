0cdce6fbc7efdc208354b7b14324e74e5613e258
Jay <godenwangzhe@gmail.com>
2022-10-19 16:56:33 +0000

    update scripts

--- ubuntu_cpu/expScripts/cloverleaf/writeFile/writeHdf5.sh
+++ ubuntu_cpu/expScripts/cloverleaf/writeFile/writeHdf5.sh
@@ -17,19 +17,23 @@ cp ${scriptsDir}/ascent_actions_relay_hdf5.yaml ascent_actions.yaml
 
 # #1.26*1.26*1.26 approximates to 2
 #MESH_SIZE_LIST="64 80 100 126"
-MESH_SIZE_LIST="64"
+MESH_SIZE_LIST="64 128 256"
 
 for MESH_SIZE in ${MESH_SIZE_LIST}
 do
 echo "test the mesh size ${MESH_SIZE}"
 cp ${scriptsDir}/clover.in_jet clover.in
-sed -i "s/end_step=600/end_step=2000/" clover.in
+#sed -i "s/end_step=600/end_step=200/" clover.in
 sed -i "s/64/${MESH_SIZE}/" clover.in
 
-./cloverleaf3d_par > sim.${MESH_SIZE}.log
-mv timing.0.out timing.0.${MESH_SIZE}.out
+mpirun -n 8 ./cloverleaf3d_par &> sim.log
 
-echo "check timing.0.51.out"
-cat timing.0.${MESH_SIZE}.out |grep ParticleAdvectionFilter |cut -d " " -f 2
-
+# put the output in specific dir
+mkdir log_${MESH_SIZE}
+mv sim.log log_${MESH_SIZE}
+mv timing.*.out log_${MESH_SIZE}
+mv testoutput* log_${MESH_SIZE}
+# also put config here to better trace the results
+cp clover.in log_${MESH_SIZE}
+cp ascent_actions.yaml log_${MESH_SIZE}
 done
